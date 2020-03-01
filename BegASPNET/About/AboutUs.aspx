db0fae1193977fbf148dce922a8b983460d3720e
mbrothen <mbrothen@gmail.com>
2020-03-01 13:01:24 -0600

    Chapter 8 complete

---
+++ BegASPNET/About/AboutUs.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="About Us" Language="C#" MasterPageFile="~/MasterPages/Frontend.master" AutoEventWireup="true" CodeFile="AboutUs.aspx.cs" Inherits="About_AboutUs" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cpMainContent" Runat="Server">
+    <p>
+        About Planet Wrox</p>
+    <p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    </p>
+    <Wrox:Banner ID="Banner2" runat="server" DisplayDirection="Horizontal" />
+</asp:Content>
+
---
+++ BegASPNET/App_Code/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+///// <summary>
+///// Summary description for RouteConfig
+///// </summary>
+//public class RouteConfig
+//{
+//    public RouteConfig()
+//    {
+//        //
+//        // TODO: Add constructor logic here
+//        //
+//    }
+//}
+using System.Web.Routing;
+using Microsoft.AspNet.FriendlyUrls;
+
+public static class RouteConfig
+{
+    public static void RegisterRoutes(RouteCollection routes)
+    {
+        var settings = new FriendlyUrlSettings();
+        settings.AutoRedirectMode = RedirectMode.Permanent;
+        routes.EnableFriendlyUrls(settings);
+    }
+}
